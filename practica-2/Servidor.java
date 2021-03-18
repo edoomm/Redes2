2dffa8982ae5a45174c47e46b808996402f5a4bc
edoomm <edoomm8@gmail.com>
2021-03-18 08:19:25 -0600

    Inicializacion proyecto

---
+++ practica-2/Servidor.java
@@ -0,0 +1,16 @@
+/**
+ * SOPA DE LETRAS
+ * Clase que recibirá clientes y escogerá y mandará un objeto de tipo Tablero 
+ * al cliente para que del lado del cliente se realice la lógica del juego
+ */
+
+public class Servidor {
+    private final String[]    tematicas = {"Redes", ""}
+    private final String[]    palabrasTematica1 = {"Protocolos", "Internet"}
+    private final String[]    palabrasTematica2 = {"Protocolos", "Internet"}
+
+    public static void main(String[] args) {
+        System.out.println("Servidor");
+        new Tablero(tematicas[n], palabrasTematica1[])
+    }
+}
