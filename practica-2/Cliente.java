a77fdb684733aa3f96374017d95416136adaa832
edoomm <edoomm8@gmail.com>
2021-03-18 15:17:16 -0600

    implementación de tematica a escoger

--- practica-2/Cliente.java
+++ practica-2/Cliente.java
@@ -1,5 +1,93 @@
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Cliente {
+    private Socket              socket;
+    private ObjectInputStream   iStream;
+    private ObjectOutputStream  oStream;
+    private Scanner             scanner;
+
+    // Constructor
+    public Cliente() {
+        iStream = null;
+        oStream = null;
+        scanner = new Scanner(System.in);
+    }
+
+    // Métodos
+
+    private void cerrarFlujos() {
+        try {
+            oStream.close();
+            iStream.close();
+            socket.close();
+            scanner.close();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.err.println("Error al cerrar los flujos");
+            e.printStackTrace();
+        }
+    }
+
+    private void elegirTematica() {
+        try {
+            int opcion = -1;
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            System.out.println("Escoja su temática:\n----------\n0) Redes\n1) Geografia\n2) Mascotas");
+            System.out.print("Opcion: ");
+            while (opcion < 0 || opcion > 2) {
+                opcion = scanner.nextInt();
+                if (opcion < 0 || opcion > 2) {
+                    System.out.println("Opcion invalida, vuelva a intentarlo");
+                    System.out.print("Opcion: ");
+                }
+            }
+            dataOutputStream.writeInt(opcion);
+
+            dataOutputStream.flush();
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+
+    private void iniciarJuego() {
+        elegirTematica();
+        try {
+            System.out.println("Igual hasta acá vamo bien jeje");
+
+            cerrarFlujos();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.err.println("Error al iniciar el juego");
+            e.printStackTrace();
+        }
+    }
+
+    private void conectarse() {
+        try {
+            int pto = 1234;
+            socket = new Socket("localhost", pto);
+
+            oStream = new ObjectOutputStream(socket.getOutputStream());
+            iStream = new ObjectInputStream(socket.getInputStream());
+
+            if (socket != null) {
+                iniciarJuego();
+            } else {
+                System.out.println("Socket indefinido. Error al conectarse");
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("Error al conectarse al servidor");
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Cliente");
+        new Cliente().conectarse();
     }
 }
--- practica-2/Servidor.java
+++ practica-2/Servidor.java
@@ -1,5 +1,6 @@
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.DataInputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
@@ -9,30 +10,73 @@ import java.io.ObjectOutputStream;
  */
 
 public class Servidor {
+    private Socket              cli;
+    private ServerSocket        serv;
     private ObjectOutputStream  oos;
     private ObjectInputStream   ois;
     private int                 tematica;
-    private final String[]      tematicas = {"Redes", ""};
+    private final String[]      tematicas = {"Redes", "Geografia", "Mascotas"};
     private final String[]      palabrasTematica1 = {"Protocolos", "Internet"};
     private final String[]      palabrasTematica2 = {"Protocolos", "Internet"};
 
-    private static void iniciarJuego() {
-        
-    }
-
-    private static void iniciar() {
+    private void cerrarFlujos() {
         try {
-            int pto = 1234;
-            ServerSocket s = new ServerSocket(pto);
-            System.out.println("Servidor iniciado... Esperando clientes... ");
-            Socket c = s.accept();
-
+            oos.close();
+            ois.close();
+            cli.close();
+            serv.close();
         } catch (Exception e) {
             //TODO: handle exception
+            System.out.println("Error al tratar de cerrar los flujos");
+        }
+    }
+
+    private void obtenerTematica() {
+        try {
+            DataInputStream dis = new DataInputStream(cli.getInputStream());
+            System.out.println("Esperando temática que escogerá el usuario...");
+            tematica = dis.readInt();
+            System.out.println("Temática escogida: " + tematicas[tematica]);
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("Error al obtener tematica");
+            e.printStackTrace();
+        }
+    }
+
+    private void iniciarJuego() {
+        obtenerTematica();
+        try {
+
+            System.out.println("Hasta aca vamo bien xd");
+
+            cerrarFlujos();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("Error al iniciar juego");
+            e.printStackTrace();
+        }
+    }
+
+    private void iniciar() {
+        try {
+            int pto = 1234;
+            serv = new ServerSocket(pto);
+
+            System.out.println("Servidor iniciado... Esperando clientes... ");
+            cli = serv.accept();
+            ois = new ObjectInputStream(cli.getInputStream());
+            oos = new ObjectOutputStream(cli.getOutputStream());
+
+            iniciarJuego();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println("Error al iniciar clase Servidor");
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        iniciar();
+        new Servidor().iniciar();
     }
 }
