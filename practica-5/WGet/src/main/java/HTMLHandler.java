392d030f8ccd06a20518704b0acda1a9d737a624
edoomm <edoomm8@gmail.com>
2021-05-19 14:53:09 -0500

    All resources can be retrieved now

--- practica-5/WGet/src/main/java/HTMLHandler.java
+++ practica-5/WGet/src/main/java/HTMLHandler.java
@@ -1,10 +1,9 @@
-
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,8 +14,7 @@ import java.util.List;
 * @version 1.0
 */
 public class HTMLHandler {
-    
-    
+
     public static String getDocumentContent( File file ) throws FileNotFoundException, IOException {
         BufferedReader fileReader = new BufferedReader(new FileReader(file));
         String fileContent = "";
@@ -27,9 +25,55 @@ public class HTMLHandler {
         return fileContent;
     }
     
-    public static List<String> getTagsContent( List<String> tags, File file) throws IOException {
-        String documentContent = getDocumentContent(file);
-        System.out.println(documentContent);
+    /**
+     * Gets every resource that should be downloaded in a file that uses these resources
+     * @param   file        The file to analyze its contents
+     * @return              The resources that should be downloaded
+     * @throws  IOException
+     */
+    public static List<String> getTagsContent(File file) throws IOException {
+        List<String> tags = null;
+
+        try {
+            String documentContent = getDocumentContent(file);
+            System.out.println(documentContent);
+            tags = getResources(documentContent);
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("Exception ocurred because the file couldn't be found");
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            System.err.println("Exception ocurred while trying to retrieve the input or output stream");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Excepction ocurred");
+            e.printStackTrace();
+        }
+
         return tags;
     }
+
+    /**
+     * Gets the name of the files that are within a document content (HTML)
+     * @param content   The document to analyze
+     * @return          A list of all names of the resources
+     */
+    private static List<String> getResources(String content) {
+        List<String> resources = new ArrayList<String>();
+        String href = "href";
+
+        // first index of the first href
+        int index = content.indexOf(href);
+        while (index >= 0) {
+            // sets start and end indexes
+            int start = content.indexOf("\"", index + 1) + 1;
+            int end = content.indexOf("\"", start + 1);
+            // getting the name of the resource
+            resources.add(content.substring(start, end));
+            // updating index in order to look for the next resource
+            index = content.indexOf(href, end + 1);
+        }
+
+        return resources;
+    }
+
 }
--- practica-5/WGet/src/main/java/WGetExecutor.java
+++ practica-5/WGet/src/main/java/WGetExecutor.java
@@ -1,16 +1,23 @@
-
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 
+/**
+ * Main class that will recieve an URL and will download its contents
+ */
 
 public class WGetExecutor {
     public static void main(String[] args) {
+        List<String> resourcesToDownload;
         try {
-            HTMLHandler.getTagsContent(null, new File("index.html"));
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            resourcesToDownload = HTMLHandler.getTagsContent(new File("index.html"));
+            System.out.println(resourcesToDownload.toString());
+        } catch (IOException ioe) {
+            System.err.println("Exception ocurred while trying to retrieve the input or output");
+            ioe.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Exception ocurred");
+            e.printStackTrace();
         }
     }
 }
